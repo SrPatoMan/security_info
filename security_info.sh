8606adad0f9408ef79b05b14f5debbea2a32d664
SrPatoMan <85370678+SrPatoMan@users.noreply.github.com>
2022-12-04 21:37:12 +0100

    Add files via upload
    HERRAMIENTA EN PROCESO DE CREACIÓN

---
+++ security_info.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Dev: Manuel Ramos
+#v 1.0
+
+
+#Comprobando privilegios SUID peligrosos (Checking dangerous SUID privileges)
+
+cd /
+find \-perm -4000 2>/dev/null
+
+
+
+########################  SIN ACABAR  #########################
